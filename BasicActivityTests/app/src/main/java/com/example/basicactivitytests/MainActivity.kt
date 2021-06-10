dc311b4f2b345c2baca8ee2cca0f624a8cbf2686
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-10 18:36:51 +0530

    Android Project ready for tes

--- BasicActivityTests/app/src/main/java/com/example/basicactivitytests/MainActivity.kt
+++ BasicActivityTests/app/src/main/java/com/example/basicactivitytests/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.basicactivitytests
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.basicactivitytests.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit private var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+
+        binding.nextButton.setOnClickListener {
+             val intent = Intent(this , SecondActivity::class.java)
+                startActivity(intent)
+        }
     }
 }
