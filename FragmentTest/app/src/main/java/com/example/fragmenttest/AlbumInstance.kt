0acf815b675426a955da08486bea1f5493c9c21b
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-12 23:59:04 +0530

    Android Fragment Test Start

--- FragmentTest/app/src/main/java/com/example/fragmenttest/AlbumInstance.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fragmenttest
-
-//Instance providing raw data
-object AlbumInstance{
-    val album = arrayListOf<Album>(
-        //Populated with album data
-    )
-}
--- FragmentTest/app/src/main/java/com/example/fragmenttest/AlbumInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fragmenttest
-
-interface AlbumInterface {
-
-    //Interface providing getter methods to AlbumDataResource
-
-    fun getMovie(id:Int):Album
-    fun getMovies():ArrayList<Album>
-}
