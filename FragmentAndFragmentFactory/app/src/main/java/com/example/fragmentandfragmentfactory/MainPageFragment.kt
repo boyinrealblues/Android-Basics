3c6face33c0de556f7d970ff42cc57f5ae3c40ce
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-15 01:28:24 +0530

    Project with no failures, bugs found

--- FragmentAndFragmentFactory/app/src/main/java/com/example/fragmentandfragmentfactory/MainPageFragment.kt
+++ FragmentAndFragmentFactory/app/src/main/java/com/example/fragmentandfragmentfactory/MainPageFragment.kt
@@ -51,8 +51,9 @@ class MainPageFragment(private val dataSource : RemoteDataSource) : Fragment() {
     private fun initViews() {
         binding.title.setText(data.name)
         binding.description.setText(resources.getString(data.description))
-        Glide.with(this@MainPageFragment)
-            .load("upload.wikimedia.org/wikipedia/commons/4/4f/BTS_for_Dispatch_White_Day_Special%2C_27_February_2019_01.jpg").centerCrop().into(binding.image)
+        Glide.with(this)
+            .load("https://upload.wikimedia.org/wikipedia/commons/4/4f/BTS_for_Dispatch_White_Day_Special%2C_27_February_2019_01.jpg").placeholder(R.drawable.bts)
+                .centerCrop().into(binding.image)
     }
 
 }
---
+++ FragmentAndFragmentFactory/app/src/main/java/com/example/fragmentandfragmentfactory/Readme.txt
@@ -0,0 +1,3 @@
+the fragments survive config changes due to dependency injection via fragment factory
+glide was not able to upload the image on the app placeholder used
+scrolling behaviour was not established as intended : to scroll above the image
