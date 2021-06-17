a89d3e7c20996d85899b75e471fce163050a3912
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-17 22:48:26 +0530

    first coroutine

--- UnderstandingCoroutines/app/src/main/java/com/example/understandingcoroutines/MainActivity.kt
+++ UnderstandingCoroutines/app/src/main/java/com/example/understandingcoroutines/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.understandingcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.understandingcoroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     lateinit private var binding : ActivityMainBinding
+
+    val scope = CoroutineScope(CoroutineName("MyScope"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +28,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateView() {
+
+        scope.launch {
+            Log.e(TAG, this.coroutineContext.toString())
+        }
         supportFragmentManager.beginTransaction().replace(R.id.container,FirstFragment::class.java,null).addToBackStack("FirstFragment").commit()
     }
 }
