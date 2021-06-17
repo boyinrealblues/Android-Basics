9db04499e7923dab0b62e589e87e4d6a62642d10
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-17 23:23:44 +0530

    coroutines in different scopes

--- UnderstandingCoroutines/app/src/main/java/com/example/understandingcoroutines/FirstFragment.kt
+++ UnderstandingCoroutines/app/src/main/java/com/example/understandingcoroutines/FirstFragment.kt
@@ -1,19 +1,23 @@
 package com.example.understandingcoroutines
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.example.understandingcoroutines.databinding.FragmentFirstBinding
-import com.example.understandingcoroutines.databinding.FragmentSecondBinding
+import kotlinx.coroutines.*
 
-
+private const val TAG = "FirstFragment"
 class FirstFragment : Fragment() {
+    var c=0
 
     lateinit private var binding : FragmentFirstBinding
 
+    val scope = CoroutineScope(CoroutineName("MyScope"))
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +31,94 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.scope1.setOnClickListener {
+            if(c==0) {
+                scopeInGlobal()
+                c++
+            }
+        }
+
+        binding.scope2.setOnClickListener {
+            if(c==0) {
+              scopeInLifecycle()
+                c++
+          }
+        }
+
+        binding.scope3.setOnClickListener {
+
+            if(c==0) {
+                scopeInCustom()
+                c++
+            }
+        }
         binding.navigate.setOnClickListener {
             parentFragmentManager.beginTransaction().replace(R.id.container,SecondFragment::class.java,null).addToBackStack("SecondFragment").commit()
         }
     }
 
+    private fun scopeInGlobal(){
+
+        GlobalScope.launch {
+            while(true){
+                Log.e(TAG,"Running. in GlobalScope..")
+                delay(1000L)
+            }
+        }
+
+    }
+
+    private fun scopeInLifecycle(){
+
+        lifecycleScope.launch {
+            while(true){
+                Log.e(TAG,"Running in LifecycleScope...")
+                delay(1000L)
+            }
+        }
+
+    }
+
+    private fun scopeInCustom(){
+
+        scope.launch {
+            while(true){
+                Log.e(TAG,"Running in CustomScope...")
+                delay(1000L)
+            }
+        }
+
+    }
+    override fun onStop() {
+        super.onStop()
+
+        Log.e(TAG,"Stopped")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.e(TAG,"Resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.e(TAG,"Paused")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e(TAG,"Destroyed")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.e(TAG,"Started")
+    }
+
+
 }
+
--- UnderstandingCoroutines/app/src/main/java/com/example/understandingcoroutines/MainActivity.kt
+++ UnderstandingCoroutines/app/src/main/java/com/example/understandingcoroutines/MainActivity.kt
@@ -5,33 +5,25 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.understandingcoroutines.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.*
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
-    lateinit private var binding : ActivityMainBinding
-
-    val scope = CoroutineScope(CoroutineName("MyScope"))
+    lateinit private var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        if(supportFragmentManager.fragments.size == 0 ){
+        if (supportFragmentManager.fragments.size == 0) {
             updateView()
         }
     }
 
     private fun updateView() {
-
-        scope.launch {
-            Log.e(TAG, this.coroutineContext.toString())
-        }
-        supportFragmentManager.beginTransaction().replace(R.id.container,FirstFragment::class.java,null).addToBackStack("FirstFragment").commit()
+        supportFragmentManager.beginTransaction().replace(R.id.container, FirstFragment::class.java, null).addToBackStack("FirstFragment").commit()
     }
 }
+
