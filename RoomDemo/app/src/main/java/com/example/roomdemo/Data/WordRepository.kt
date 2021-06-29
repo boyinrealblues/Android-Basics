70c2c475d54e2883286cd413d59d4de6cd0a44de
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-29 23:27:31 +0530

    repository made

---
+++ RoomDemo/app/src/main/java/com/example/roomdemo/Data/WordRepository.kt
@@ -0,0 +1,21 @@
+package com.example.roomdemo.Data
+
+import androidx.annotation.WorkerThread
+import com.example.roomdemo.Room.Word
+import com.example.roomdemo.Room.WordDao
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository(private val wordDao: WordDao){
+
+    val wordList : Flow<List<Word>> = wordDao.getAllWords()
+
+    @WorkerThread
+    suspend fun insert(word : Word){
+        wordDao.insert(word)
+    }
+
+    @WorkerThread
+    suspend fun delete(){
+        wordDao.deleteEverything()
+    }
+}
