5a706c1a564c6cf606fa18c6488492b2db62a8fc
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-06-30 00:00:08 +0530

    adapter , viewholder , layout_viewholder made

---
+++ RoomDemo/app/src/main/java/com/example/roomdemo/UI/WordAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.roomdemo.UI
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdemo.R
+import com.example.roomdemo.Room.Word
+
+class WordAdapter : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    private var oldlist = emptyList<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+
+        holder.bind(oldlist[position])
+
+    }
+
+    override fun getItemCount(): Int  = oldlist.size
+
+     class WordViewHolder(private val view : View) : RecyclerView.ViewHolder(view){
+        private val textView : TextView
+         init{
+             textView = view.findViewById<TextView>(R.id.text)
+        }
+         fun bind(item : String){
+             textView.setText(item)
+         }
+
+
+        companion object{
+            fun create(parent : ViewGroup) : WordViewHolder{
+               val view  = LayoutInflater.from(parent.context).inflate(R.layout.view_holder_layout,parent,false)
+                return WordViewHolder(view)
+            }
+        }
+    }
+
+        fun submitList(newList : List<String>){
+            val diff = WordDiffUtil(oldList = oldlist ,newList)
+            oldlist = newList
+            val calc = DiffUtil.calculateDiff(diff)
+            calc.dispatchUpdatesTo(this)
+        }
+}
---
+++ RoomDemo/app/src/main/java/com/example/roomdemo/UI/WordDiffUtil.kt
@@ -0,0 +1,16 @@
+package com.example.roomdemo.UI
+
+import androidx.recyclerview.widget.DiffUtil
+
+class WordDiffUtil(private val oldList : List<String> , private val newList : List<String> ) : DiffUtil.Callback() {
+    override fun getOldListSize() = oldList.size
+    override fun getNewListSize() = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
